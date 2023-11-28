a6faa4d65d149b9572def8829d37495b36559085
fespasa <ferranespasa@gmail.com>
2023-11-28 13:12:40 +0100

    Set the focus at the  first input

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -1,1 +1,6 @@
-console.log("Test");
+//Connection TEST - console.log("Test"); - SUCCESSFUL
+
+/* add focus at the first input when the page loads */
+window.addEventListener("load", () => {
+    document.querySelector('#name').focus();
+});
