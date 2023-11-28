57dafa7005cf9f874100dd45ea9827108b7e7a41
fespasa <ferranespasa@gmail.com>
2023-11-28 17:49:17 +0100

    T-Shirt info section

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -2,18 +2,33 @@
 
 const jobRoleField = document.querySelector('#title');
 const otherJobRoleField = document.querySelector('#other-job-role');
+const colorField = document.querySelector('#color');
+const designField = document.querySelector('#design');
 
 /* add focus at the first input when the page loads */
 window.addEventListener("load", () => {
     document.querySelector('#name').focus();
     otherJobRoleField.style.display = "none"; //hide optional Other Job Role field
+    colorField.disabled = true; //color field disabled
 });
 
 /* show or hide "other job role" input in case of the value of the Job Role selector */
-jobRoleField.addEventListener("change", (e) => {
+jobRoleField.addEventListener("change", () => {
     if(jobRoleField.value === "other") {
         otherJobRoleField.style.display = "block";
     } else {
         otherJobRoleField.style.display = "none";
+    }
+});
+
+/* show only the colors of each model of t-shirt */
+designField.addEventListener("change", (e) => {
+    colorField.disabled = false;
+    for (i = 1; i < colorField.children.length; i++) {
+        if(e.target.value === colorField.children[i].getAttribute("data-theme")){
+            colorField.children[i].removeAttribute("hidden");
+        } else {
+            colorField.children[i].setAttribute("hidden", "");
+        }
     }
 });
