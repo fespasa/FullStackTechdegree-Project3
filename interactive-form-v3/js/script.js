e48b3532b4f57d4cc1cf1030cf0a14c8086eaba9
fespasa <ferranespasa@gmail.com>
2023-11-28 13:54:41 +0100

    Job Role Section

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -1,6 +1,19 @@
 //Connection TEST - console.log("Test"); - SUCCESSFUL
 
+const jobRoleField = document.querySelector('#title');
+const otherJobRoleField = document.querySelector('#other-job-role');
+
 /* add focus at the first input when the page loads */
 window.addEventListener("load", () => {
     document.querySelector('#name').focus();
+    otherJobRoleField.style.display = "none"; //hide optional Other Job Role field
+});
+
+/* show or hide "other job role" input in case of the value of the Job Role selector */
+jobRoleField.addEventListener("change", (e) => {
+    if(jobRoleField.value === "other") {
+        otherJobRoleField.style.display = "block";
+    } else {
+        otherJobRoleField.style.display = "none";
+    }
 });
