d4d32891add86f7baada35b3d002e51c79389f55
fespasa <ferranespasa@gmail.com>
2023-11-30 13:45:29 +0100

    Form Validation

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -6,8 +6,9 @@ const colorField = document.querySelector('#color');
 const designField = document.querySelector('#design');
 const activitiesFieldset = document.querySelector('#activities');
 const paymentField = document.querySelector('#payment');
+const conferenceForm = document.querySelector('form');
 
-/* add focus at the first input when the page loads */
+/*===== add focus at the first input when the page loads =====*/
 window.addEventListener("load", () => {
     document.querySelector('#name').focus();
     otherJobRoleField.style.display = "none"; //hide optional Other Job Role field
@@ -17,7 +18,7 @@ window.addEventListener("load", () => {
     document.querySelector('#bitcoin').setAttribute("hidden", "");
 });
 
-/* show or hide "other job role" input in case of the value of the Job Role selector */
+/*===== show or hide "other job role" input in case of the value of the Job Role selector =====*/
 jobRoleField.addEventListener("change", () => {
     if(jobRoleField.value === "other") {
         otherJobRoleField.style.display = "block";
@@ -26,7 +27,7 @@ jobRoleField.addEventListener("change", () => {
     }
 });
 
-/* show only the colors of each model of t-shirt */
+/*===== show only the colors of each model of t-shirt =====*/
 designField.addEventListener("change", (e) => {
     colorField.disabled = false;
     colorField.firstElementChild.removeAttribute("selected");
@@ -40,7 +41,7 @@ designField.addEventListener("change", (e) => {
     document.querySelector(`[data-theme='${e.target.value}']`).setAttribute("selected", "");
 });
 
-/* Activities Section */
+/*===== Activities Section =====*/
 let totalCost = 0;
 activitiesFieldset.addEventListener("change", (e) => {
     if (e.target.hasAttribute("selected")) {
@@ -53,7 +54,7 @@ activitiesFieldset.addEventListener("change", (e) => {
     document.querySelector('#activities-cost').innerHTML = `Total: $${totalCost}`;
 });
 
-/* Payment info Section */
+/*===== Payment info Section =====*/
 paymentField.addEventListener("change", (e) => {
     const methodsBoxes = e.target.parentNode.parentNode.children;
     // I use a bucle to show or hide. If you add more payment methods it will work properly.
@@ -63,5 +64,49 @@ paymentField.addEventListener("change", (e) => {
         } else {
             methodsBoxes[i].setAttribute("hidden", "");
         }
+    }
+});
+
+/*===== Form Validation =====*/
+conferenceForm.addEventListener("submit", (e) => {
+    const isFieldEmpty = (field) => {
+        const fieldValue = field.value.trim();
+        return fieldValue === "";
+    }
+    const isEmailValid = (field) => {
+        const fieldValue = field.value.trim();
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(fieldValue);
+    }
+    const noSubmit = (condition) => {
+        if(condition) {
+            e.preventDefault();
+        }
+    }
+
+    /* Name Validation - Not empty or blank */
+    noSubmit(isFieldEmpty(document.querySelector('#name')));
+
+    /* Email Validation - Correct email format */
+    noSubmit(!isEmailValid(document.querySelector('#email')));
+
+    /* Activities Validation - At least one selected */
+    const activitiesInputs = document.querySelectorAll('#activities input');
+    let activitiesCounter = 0;
+    for (let i = 0; i < activitiesInputs.length; i++){
+        if (activitiesInputs[i].hasAttribute("selected")){
+            activitiesCounter++;
+        }
+    }
+    noSubmit(activitiesCounter === 0);
+
+    /* Credit Card Validation - 13 to 16 digit CC number */
+    const ccNumRegex = /^\d{13,16}$/;
+    const ccZipRegex = /^\d{5}$/;
+    const ccCvvRegex = /^\d{3}$/;
+    if(document.querySelector('#payment').value === "credit-card") {
+        noSubmit(!ccNumRegex.test(document.querySelector('#cc-num').value)); // CC nº field validation
+        noSubmit(!ccZipRegex.test(document.querySelector('#zip'))); // zip field validation
+        noSubmit(!ccCvvRegex.test(document.querySelector('#cvv'))); // cvv field validation
     }
 });
